804d0c05f3e75ca39949a4d6fc65e86c0396aaa9
Lennart L <lennart.lehmann@student.hpi.de>
2016-10-24 18:57:04 +0200

    failing test for #3

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -5,4 +5,9 @@ describe "Authors page", :type => :feature do
   it "should render" do
     visit "authors/new"
   end
+
+  it "should have a field 'First name'" do
+  	visit "authors/new"
+  	expect(page).to have_field(first_name)
+  end
 end
