243e30fc8eee47a917bfb19df79c7746e312685a
Lennart L <lennart.lehmann@student.hpi.de>
2016-11-13 14:33:50 +0100

    added show method, params.require, failing test for #13

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -18,8 +18,18 @@ describe "Authors page", :type => :feature do
   	expect(page).to have_button("Create Author")
   end
 
-  it "should create a instance of Author after entering data" do
+  it "should create an instance of Author after entering data" do
     @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
     expect(@author).not_to be_nil
+  end
+
+  it "should be found in database" do
+  	visit "authors/new"
+  	@author = build(:author)
+  	fill_in "First name", with: @author.first_name
+  	fill_in "Last name", with: @author.first_name
+  	fill_in "Homepage", with: @author.first_name
+  	click_button "Create Author"
+  	expect(Author.find_by(first_name: @author.first_name, last_name: @author.last_name, homepage: @author.homepage)).not_to be_nil
   end
 end
