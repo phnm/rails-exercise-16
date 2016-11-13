f6e8aab03657eb655c929ccb0a5019ebffe3ac46
Lennart L <lennart.lehmann@student.hpi.de>
2016-11-13 13:35:25 +0100

    fix

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   it "should be the full name" do
-  	expect(build(:author).name.to eq('Alan Turing'))
+  	expect(build(:author).name).to eq('Alan Turing')
   end
 end
