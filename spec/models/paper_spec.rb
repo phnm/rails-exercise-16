9ed774fbd71da091beb15b0283bce27ca2fc7ea7
Lennart L <lennart.lehmann@student.hpi.de>
2016-11-13 22:00:55 +0100

    failing test for #39

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-    it "should not validate without title and venue" do
+    it "should not validate without title and venue and year" do
 	    expect(Paper.new(title: "alan", venue: "", year: 1999).save).to be false
 	    expect(Paper.new(title: nil, venue: "dort", year: 1999).save).to be false
+	    expect(Paper.new(title: "alan", venue: "dort", year: nil).save).to be false
+	    expect(Paper.new(title: "alan", venue: "dort", year: "#").save).to be false
    end
 end
