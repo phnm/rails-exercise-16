af58cc0cd1ddfd2291accee4f82c8c062f2008d5
Lennart L <lennart.lehmann@student.hpi.de>
2016-11-13 21:56:57 +0100

    added model test. closes #37, closes #38

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "should not validate without title and venue" do
+	    expect(Paper.new(title: "alan", venue: "", year: 1999).save).to be false
+	    expect(Paper.new(title: nil, venue: "dort", year: 1999).save).to be false
+   end
 end
